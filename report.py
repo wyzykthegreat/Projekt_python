59f78f0d49baab99d569911d03427f03de78e7e8
wyzyk <wyzykjr@gmail.com>
2021-05-11 17:17:32 +0200

    test

---
+++ report.py
@@ -0,0 +1,9 @@
+from data import Graph_data
+from exceptions import UnableToGenerateReportException
+
+class Report():
+    def __init__(self):
+        pass
+
+    def create_report(self):
+        pass
